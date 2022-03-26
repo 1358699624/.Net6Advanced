cf49eb7bc2eb8a299a7e28b052be380591c5c1d1
ma <1358699624@qq.com>
2022-03-26 17:04:55 +0800

    新增WebApi+Swagger111

---
+++ Advanced.Net6.WebApi/Controllers/Firstv2Controller.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Advanced.Net6.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName=nameof(ApiVersionInfo.V2))]
+    public class Firstv2Controller : ControllerBase
+    {
+        /// <summary>
+        /// 获取数据
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/<FirstController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// 根据id获取数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/<FirstController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// 提交数据
+        /// </summary>
+        /// <param name="value"></param>
+        // POST api/<FirstController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// 修改数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        // PUT api/<FirstController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE api/<FirstController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Advanced.Net6.WebApi/Controllers/Utilty/ApiVersionInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Advanced.Net6.WebApi.Controllers
+{
+    public class ApiVersionInfo
+    {
+        public static string V1;
+        public static string V2;
+        public static string V3;
+        public static string V4;
+    }
+}
