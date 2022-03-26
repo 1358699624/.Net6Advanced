ade7d0dcf60aff976fc8d8913f9dfe4183ce768b
ma <1358699624@qq.com>
2022-03-26 17:04:55 +0800

    111

---
+++ Advanced.Net6.Project/Controllers/SixthController.cs
@@ -0,0 +1,18 @@
+﻿using Advanced.Net6.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Advanced.Net6.Project.Controllers
+{
+    public class SixthController : Controller
+    {
+
+        public SixthController(IMircPhone mircPhone,IServiceProvider serviceProvider) 
+        { 
+        
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
